ef3f4c98bcfd4cc1cd53945f587807501b003e61
Atluri Sri Charitha <sricharitha06atluri@gmail.com>
2026-07-08 22:53:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,9 @@ Q1_ALLOWED_ORIGIN = "https://dash-ld4zbx.example.com"
 # 3. Q2: OAuth JWKS (Issuer, Audience, and Public Key)
 ISSUER = "https://idp.exam.local"
 AUDIENCE = "tds-jtqp6gsu.apps.exam.local"
-PUBLIC_KEY_PEM = -----BEGIN PUBLIC KEY-----
+PUBLIC_KEY_PEM = """-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+... -----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
 cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
 EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
@@ -20,7 +22,8 @@ WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
 ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
 SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
 dQIDAQAB
------END PUBLIC KEY-----
+-----END PUBLIC KEY----- ...
+-----END PUBLIC KEY-----"""
 
 # 4. Q3: 12-Factor Config (Manually merge the variables)
 Q3_PORT =8981
